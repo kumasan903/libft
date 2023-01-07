2922c6e0f7b8fef18ce23f2849371ea67fe9a5a7
skawanis <skawanis@student.42tokyo.jp>
2023-01-07 09:26:01 +0000

    norminette

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skawanis <skawanis@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/07 09:25:19 by skawanis          #+#    #+#             */
+/*   Updated: 2023/01/07 09:25:28 by skawanis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 // TODO　多分destとかsrcがNULLだったときにセグフォするからそのエラー処理。
-void *ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(i < n)
+	while (i < n)
 	{
 		*((unsigned char *)dest + i) = *((unsigned char *)src + i);
 		i++;
