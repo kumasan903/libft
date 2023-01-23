bbc87af2bdfd797e19e1b7a91c25c6b0afc8251c
skawanis <skawanis@student.42tokyo.jp>
2023-01-23 05:28:36 +0000

    strncmp unsigned

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: skawanis <skawanis@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 23:48:19 by skawanis          #+#    #+#             */
-/*   Updated: 2023/01/17 17:46:34 by skawanis         ###   ########.fr       */
+/*   Updated: 2023/01/23 05:23:36 by skawanis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
