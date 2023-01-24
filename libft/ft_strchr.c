ea956fadf3732c6d7d7e4d962a9150f20643b4f0
skawanis <skawanis@student.42tokyo.jp>
2023-01-24 14:54:27 +0000

    ft_strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: skawanis <skawanis@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 11:56:08 by skawanis          #+#    #+#             */
-/*   Updated: 2023/01/24 14:52:31 by skawanis         ###   ########.fr       */
+/*   Updated: 2023/01/24 14:54:20 by skawanis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ char	*ft_strchr(const char *s, int c)
 	while (str[i])
 	{
 		if (str[i] == chr)
-			return (str + i);
+			return (&str[i]);
 		i++;
 	}
 	if (str[i] == chr)
 	{
-		return (str + i);
+		return (&str[i]);
 	}
 	return (NULL);
 }
