4dc6563c72656618721c2c8ae180b765be84b8a2
skawanis <skawanis@student.42tokyo.jp>
2023-01-24 16:44:51 +0000

    strrchr

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: skawanis <skawanis@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 11:56:08 by skawanis          #+#    #+#             */
-/*   Updated: 2023/01/24 16:42:56 by skawanis         ###   ########.fr       */
+/*   Updated: 2023/01/24 16:44:40 by skawanis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	size_t		i;
+	size_t			i;
 	unsigned char	*str;
-	unsigned char	*return_value;
+	char			*return_value;
 
 	str = (unsigned char *)s;
 	i = 0;
