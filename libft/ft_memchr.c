83d7ebbba7824a57ac1bee9d12620b82efa64c7b
skawanis <skawanis@student.42tokyo.jp>
2023-01-24 08:43:51 +0000

    ft_memchr

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: skawanis <skawanis@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 11:56:08 by skawanis          #+#    #+#             */
-/*   Updated: 2023/01/11 04:53:41 by skawanis         ###   ########.fr       */
+/*   Updated: 2023/01/24 08:43:37 by skawanis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memchr(const void *buf, int ch, size_t n)
 	size_t	i;
 	char	*str;
 
-	str = (char *)buf;
+	str = (unsigned char *)buf;
 	i = 0;
 	while (i < n)
 	{
