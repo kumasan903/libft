c7f6e237582f8a32826da35900119f110866ce53
skawanis <skawanis@student.42tokyo.jp>
2023-01-28 07:20:48 +0000

    itoa long

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,13 +6,13 @@
 /*   By: skawanis <skawanis@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 07:11:28 by skawanis          #+#    #+#             */
-/*   Updated: 2023/01/28 07:11:58 by skawanis         ###   ########.fr       */
+/*   Updated: 2023/01/28 07:20:13 by skawanis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	count_num_len(int n)
+static int	count_num_len(long n)
 {
 	size_t	n_len;
 
@@ -29,28 +29,37 @@ static int	count_num_len(int n)
 
 char	*ft_itoa(int n)
 {
+	long	long_n;
 	size_t	n_len;
 	short	sign;
 	char	*str;
 
+	long_n = n;
 	sign = 1;
-	n_len = count_num_len(n);
-	if (n < 0)
+	n_len = count_num_len(long_n);
+	if (long_n < 0)
 	{
 		sign *= -1;
-		n *= -1;
+		long_n *= -1;
 		n_len ++;
 	}
 	str = ft_calloc(sizeof(char), n_len + 1);
-	if (n == 0)
+	if (long_n == 0)
 		str[0] = '0';
-	while (n)
+	while (long_n)
 	{
-		str[n_len - 1] = n % 10 + '0';
-		n /= 10;
+		str[n_len - 1] = long_n % 10 + '0';
+		long_n /= 10;
 		n_len--;
 	}
 	if (sign == -1)
 		str[0] = '-';
 	return (str);
+}
+
+#include <stdio.h>
+#include <limits.h>
+int	main(void)
+{
+	printf("%s\n", ft_itoa(INT_MIN));
 }
