f45ec0e92937de1dc0aecbd60d7120d4713abf47
skawanis <skawanis@student.42tokyo.jp>
2023-02-21 19:16:55 +0000

    何日libftサボってたんだろう。とりあえずputnbrできた。はず。

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,14 +6,71 @@
 /*   By: skawanis <skawanis@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 07:32:21 by skawanis          #+#    #+#             */
-/*   Updated: 2023/02/02 00:44:05 by skawanis         ###   ########.fr       */
+/*   Updated: 2023/02/21 19:16:19 by skawanis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static size_t	count_num_len(unsigned int nb)
+{
+	int	count;
+
+	count = 0;
+	while (nb > 0)
+	{
+		count++;
+		nb /= 10;
+	}
+	return (count);
+}
+
+static unsigned int	calc_divisor(size_t num_len)
+{
+	unsigned int	divisor;
+	int				i;
+
+	divisor = 1;
+	i = 1;
+	while (i < num_len)
+	{
+		divisor *= 10;
+		i++;
+	}
+	return (divisor);
+}
+
+static void	ft_putuint_fd(unsigned int nb, int fd)
+{
+	size_t			nb_len;
+	unsigned int	divisor;
+	int				i;
+	char			c;
+
+	nb_len = count_num_len(nb);
+	divisor = calc_divisor(nb_len);
+	i = 0;
+	while (i < nb_len)
+	{
+		c = (nb / divisor) % 10 + '0';
+		write(fd, &c, 1);
+		divisor /= 10;
+		i++;
+	}
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	// longにキャストして、マイナスだったら-1かける
-	// そしてn_lenもとめて、１番上の桁から出力していく
+	unsigned int	nb;
+
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		nb = n * -1;
+	}
+	else
+	{
+		nb = n;
+	}
+	ft_putuint_fd(nb, fd);
 }
