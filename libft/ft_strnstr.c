01fe62f504b2942b6c9dbba4cd0e293169892b4f
skawanis <skawanis@student.42tokyo.jp>
2023-02-25 08:43:57 +0000

    fix strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: skawanis <skawanis@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 05:29:34 by skawanis          #+#    #+#             */
-/*   Updated: 2023/01/11 06:07:55 by skawanis         ###   ########.fr       */
+/*   Updated: 2023/02/25 08:37:10 by skawanis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	unsigned int	i;
 	unsigned int	j;
 
+	if (big == NULL)
+		return (NULL); //ほんとうにNULLでいいのかわからんけどとりあえずこれで。
 	if (little[0] == '\0')
 		return ((char *)big);
 	i = 0;
