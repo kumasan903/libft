f6b0759181d23b24df4ab702617295053de64028
Souta Kawanishi <skawanis@c1r24s10.42tokyo.jp>
2023-02-27 18:04:52 +0900

    fix ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: skawanis <skawanis@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 05:29:34 by skawanis          #+#    #+#             */
-/*   Updated: 2023/02/25 08:37:10 by skawanis         ###   ########.fr       */
+/*   Updated: 2023/02/27 18:04:11 by skawanis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	unsigned int	i;
 	unsigned int	j;
 
-	if (big == NULL)
-		return (NULL); //ほんとうにNULLでいいのかわからんけどとりあえずこれで。
 	if (little[0] == '\0')
 		return ((char *)big);
+	if (len == 0)
+		return (0);
 	i = 0;
 	while (big[i] && i < len)
 	{
