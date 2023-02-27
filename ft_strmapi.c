5a4e363087de3fb9e1b94d7d05e980a3a2069c12
skawanis <skawanis@student.42tokyo.jp>
2023-02-27 15:30:52 +0000

    create ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skawanis <skawanis@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/27 15:11:17 by skawanis          #+#    #+#             */
+/*   Updated: 2023/02/27 15:29:51 by skawanis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*new_string;
+	int		i;
+
+	new_string = ft_calloc(sizeof(char *), ft_strlen(s) + 1);
+	if (new_string == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		new_string[i] = f(i, s[i]);
+		i++;
+	}
+	return (new_string);
+}
