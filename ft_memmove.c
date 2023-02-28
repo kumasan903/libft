22eee7895f21107f5038fad0a7b74ba4e769f016
skawanis <skawanis@student.42tokyo.jp>
2023-02-28 13:20:14 +0000

    fix memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: skawanis <skawanis@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 11:35:36 by skawanis          #+#    #+#             */
-/*   Updated: 2023/01/27 21:11:25 by skawanis         ###   ########.fr       */
+/*   Updated: 2023/02/28 13:20:07 by skawanis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	}
 	else
 	{
-		i = len;
+		i = len - 1;
 		while (i >= 0)
 		{
 			*((unsigned char *)dst + i) = *((unsigned char *)src + i);
