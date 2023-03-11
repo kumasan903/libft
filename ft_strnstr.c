f67bf30549ecbcdc767627cd74258c8b37e19bfc
skawanis <skawanis@student.42tokyo.jp>
2023-03-11 11:12:42 +0000

    fix ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -27,7 +27,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		j = 0;
 		if (haystack[i] == needle[j])
 		{
-			while (i + j < len && haystack[i + j] == needle[j])
+			while (haystack[i + j] != '\0' && i + j < len && haystack[i + j] == needle[j])
 			{
 				j++;
 				if (needle[j] == '\0')
