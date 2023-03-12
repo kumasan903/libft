9687b750c7e055ca9e40dec260fc79aba51a97ae
skawanis <skawanis@student.42tokyo.jp>
2023-03-12 09:02:07 +0000

    rewrite ft_split

--- ft_split.c
+++ ft_split.c
@@ -5,119 +5,158 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: skawanis <skawanis@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/21 19:48:12 by skawanis          #+#    #+#             */
-/*   Updated: 2023/03/06 09:50:44 by skawanis         ###   ########.fr       */
+/*   Created: 2023/03/12 06:52:02 by skawanis          #+#    #+#             */
+/*   Updated: 2023/03/12 09:00:55 by skawanis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static unsigned int	count_char(char const *s, char c)
+size_t	count_word(char const *s, char c)
 {
-	unsigned int	i;
-	unsigned int	count;
+	size_t	i;
+	size_t	count;
 
+	if (s == NULL)
+		return (0);
 	i = 0;
 	count = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
-		{
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
 			count++;
-			while (s[i + 1] == c)
-				i++;
-		}
 		i++;
 	}
 	return (count);
 }
 
-static void	all_free(char ****result, unsigned int i)
+size_t	malloc_words2(char const *s, char c, size_t *offset)
 {
-	while (i >= 0)
+	size_t	word_len;
+
+	word_len = 0;
+	while (s[*offset] != c && s[*offset] != '\0')
 	{
-		free ((*result)[i]);
-		i--;
+		(*offset)++;
+		word_len++;
 	}
-	free (*result);
-	*result = NULL;
+	return (word_len);
 }
 
-static size_t	malloc_str(char const *s, char c, char **dest)
+int	malloc_words(char **return_vec, size_t word_count, char const *s, char c)
 {
-	char	*str;
-	size_t	str_len;
+	size_t	vec_i;
+	size_t	offset;
+	size_t	start;
+	size_t	word_len;
+
+	vec_i = 0;
+	offset = 0;
+	start = 0;
+	while (vec_i < word_count)
+	{
+		start = offset;
+		while (s[offset] == c)
+		{
+			offset++;
+			start++;
+		}
+		word_len = malloc_words2(s, c, &offset);
+		return_vec[vec_i] = malloc(sizeof(char) * (word_len + 1));
+		if (return_vec[vec_i] == NULL)
+			return (1);
+		ft_strlcpy(return_vec[vec_i], &s[start], word_len + 1);
+		vec_i++;
+	}
+	return (0);
+}
+
+void	*free_and_return_null(char **return_vec)
+{
 	size_t	i;
 
 	i = 0;
-	str_len = 0;
-	while (s[i] != '\0' && s[i] != c)
+	while (return_vec[i] != NULL)
 	{
-		str_len++;
+		free(return_vec[i]);
 		i++;
 	}
-	str = malloc (sizeof(char) * (str_len + 1));
-	if (str == NULL)
-	{
-		*dest = NULL;
-		return (0);
-	}
-	ft_strlcpy (str, s, str_len + 1);
-	*dest = str;
-	return (str_len);
-}
-
-static void
-	wrap_malloc_str(char ***result, char **new_s, size_t *count, char *c)
-{
-	unsigned int	i;
-	unsigned int	offset;
-
-	i = 0;
-	offset = 0;
-	while (i < *count + 1)
-	{
-		offset += malloc_str (*new_s + offset, *c, &(*result)[i]);
-		if ((*result)[i] == NULL)
-		{
-			all_free (&result, i);
-			return ;
-		}
-		while ((*new_s)[offset] == *c)
-			offset++;
-		i++;
-	}
+	free(return_vec);
+	return (NULL);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	size_t	count;
-	char	**result;
-	char	*new_s;
-	char	new_c[2];
+	const size_t	word_count = count_word(s, c);
+	char			**return_vec;
 
 	if (s == NULL)
 		return (NULL);
 	if (s[0] == '\0')
 		return (ft_calloc(sizeof(char *), 1));
-	new_c[0] = c;
-	new_c[1] = '\0';
-	new_s = ft_strtrim(s, new_c);
-	if (new_s == NULL)
+	return_vec = ft_calloc(sizeof(char *), word_count + 1);
+	if (return_vec == NULL)
 		return (NULL);
-	count = count_char (new_s, c);
-	if (count == ft_strlen (new_s))
-	{
-		free(new_s);
-		return (ft_calloc (sizeof(char *), 1));
-	}
-	result = ft_calloc (sizeof(char *), count + 2);
-	if (result == NULL)
-	{
-		free(new_s);
-		return (NULL);
-	}
-	wrap_malloc_str (&result, &new_s, &count, &c);
-	free(new_s);
-	return (result);
+	if (malloc_words(return_vec, word_count, s, c) == 1)
+		return (free_and_return_null(return_vec));
+	return (return_vec);
 }
+
+/*
+#include <stdio.h>
+int main(void)
+{
+	size_t	i;
+	char	**result;
+	result = ft_split(NULL, '\0');
+	printf("%p\n", result);
+	puts("---");
+	result = ft_split("//aaa//bb///ccccc", '/');
+	i = 0;
+	while (result[i] != NULL)
+	{
+		printf("%s\n", result[i]);
+		i++;
+	}
+	puts("---");
+	result = ft_split("test", '/');
+	i = 0;
+	while (result[i] != NULL)
+	{
+		printf("%s\n", result[i]);
+		i++;
+	}
+	puts("---");
+	result = ft_split("test", '\0');
+	i = 0;
+	while (result[i] != NULL)
+	{
+		printf("%s\n", result[i]);
+		i++;
+	}
+	puts("---");
+	result = ft_split("aaa//bb///ccccc", '/');
+	i = 0;
+	while (result[i] != NULL)
+	{
+		printf("%s\n", result[i]);
+		i++;
+	}
+	puts("---");
+	result = ft_split("//aaa//bb///ccccc///", '/');
+	i = 0;
+	while (result[i] != NULL)
+	{
+		printf("%s\n", result[i]);
+		i++;
+	}
+	puts("---");
+	result = ft_split("//////////////", '/');
+	i = 0;
+	while (result[i] != NULL)
+	{
+		printf("%s\n", result[i]);
+		i++;
+	}
+}
+*/
