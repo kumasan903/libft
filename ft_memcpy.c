6d78237dc709d0fb05758d24d82a5a0121a63a83
skawanis <skawanis@student.42tokyo.jp>
2023-03-15 12:32:20 +0000

    ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -14,16 +14,17 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t	i;
+	const unsigned char		*uchar_src = (unsigned char *)src;
+	unsigned char			*uchar_dst;
+	size_t					i;
 
-	if (dst == NULL && src == NULL)
-		return (NULL);
+	uchar_dst = (unsigned char *)dst;
 	if (dst == src)
 		return (dst);
 	i = 0;
 	while (i < n)
 	{
-		*((unsigned char *)dst + i) = *((unsigned char *)src + i);
+		uchar_dst[i] = uchar_src[i];
 		i++;
 	}
 	return (dst);
