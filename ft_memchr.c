df6a05a6bb64a410cba50df8ef1c309884ff0191
skawanis <skawanis@student.42tokyo.jp>
2023-03-15 12:21:38 +0000

    memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -15,11 +15,9 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t			i;
-	unsigned char	*str;
-	unsigned char	chr;
+	const unsigned char	*str = (unsigned char *)s;
+	const unsigned char	chr = (unsigned char)c;
 
-	str = (unsigned char *)s;
-	chr = (unsigned char)c;
 	i = 0;
 	while (i < n)
 	{
