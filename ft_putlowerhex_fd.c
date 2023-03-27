560f519db619baf9b7d0c9d86d451ca72e8d3c3c
skawanis <skawanis@student.42tokyo.jp>
2023-03-27 20:36:03 +0000

    norminette

--- ft_putlowerhex_fd.c
+++ ft_putlowerhex_fd.c
@@ -6,7 +6,7 @@
 /*   By: skawanis <skawanis@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 19:20:42 by skawanis          #+#    #+#             */
-/*   Updated: 2023/03/27 20:11:07 by skawanis         ###   ########.fr       */
+/*   Updated: 2023/03/27 20:35:59 by skawanis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ static void	rec(size_t n, int fd, size_t *count)
 
 ssize_t	ft_putlowerhex_fd(size_t n, int fd)
 {
-	size_t count;
+	size_t	count;
 
 	count = 0;
 	rec(n, fd, &count);
