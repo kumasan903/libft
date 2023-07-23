e3a860f38c3ea7c7eae258a0282024740619d1e8
skawanis <skawanis@student.42tokyo.jp>
2023-07-24 02:46:44 +0900

    fix ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: skawanis <skawanis@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 11:56:08 by skawanis          #+#    #+#             */
-/*   Updated: 2023/01/24 14:57:00 by skawanis         ###   ########.fr       */
+/*   Updated: 2023/07/24 02:06:09 by skawanis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strchr(const char *s, int c)
 	size_t		i;
 	const char	chr = (char)c;
 
+	if (s == NULL)
+		return (NULL);
 	i = 0;
 	while (s[i] != '\0')
 	{
