1f55af222777eb89427b3f3aab79fd1ed4483f72
skawanis <skawanis@student.42tokyo.jp>
2023-07-24 02:46:22 +0900

    fix ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: skawanis <skawanis@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 08:26:40 by skawanis          #+#    #+#             */
-/*   Updated: 2023/02/27 18:17:37 by skawanis         ###   ########.fr       */
+/*   Updated: 2023/07/24 02:44:40 by skawanis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,18 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s1_len;
 	size_t	s2_len;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	dest = ft_calloc(s1_len + s2_len + 1, sizeof(char));
+	if (s1 == NULL)
+		return (ft_strcdup(s2, '\0'));
+	if (s2 == NULL)
+		return (ft_strcdup(s1, '\0'));
+	s1_len = ft_strlen_s(s1);
+	s2_len = ft_strlen_s(s2);
+	dest = malloc(s1_len + s2_len + 1);
 	if (dest == NULL)
 		return (NULL);
+	dest[s1_len + s2_len] = '\0';
 	ft_memcpy(dest, s1, s1_len);
 	ft_memcpy(dest + s1_len, s2, s2_len);
 	return (dest);
